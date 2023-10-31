41bb0fee6186624bf29e3b6ebe067842340e52b3
omer-khaled <omerkhaledmohamedmohamed123@gmail.com>
2023-10-31 22:44:27 +0200

    first commit last last

--- src/utils/protectRoute.tsx
+++ src/utils/protectRoute.tsx
@@ -2,13 +2,15 @@ import { ReactElement } from "react"
 import { useSelector } from "react-redux"
 import { reducerType } from "../store/store"
 import UnAuthrized from "../components/UnAuthrized"
+import { Loading } from "../main"
 
 const protectRoute = (Oldcomponent:()=>ReactElement)=>{
     const NewComponenet = ():ReactElement=>{
         const auth = useSelector<reducerType>(state=>state.auth.auth);
+        const user = useSelector<reducerType>(state=>state.auth.user);
         return(
             <>{
-                (auth)?<Oldcomponent />:<UnAuthrized/>
+                (auth)?((user)?<Oldcomponent />:<Loading/>):<UnAuthrized/>
             }</>
         )
     }
